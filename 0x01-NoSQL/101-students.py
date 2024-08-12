123d0ef6f4d3ecc7d754fd777830746a825cfaf6
Adeoye1996 <Abdulazeezbabatunde10@gmail.com>
2024-08-12 21:37:17 +0000

    101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,21 @@
-""" The function that returns all students sorted by average score: """
-
+#!/usr/bin/env python3
+""" Python Module for 101-students.py
+ """
 
 def top_students(mongo_collection):
-    """ Average score must be part of each item returns with key = averageScore"""
-    return list(mongo_collection.find())
+    """ Returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': '$name',
+                'averageScore': {
+                    '$avg': "$topics.score"
+                }
+            }
+        },
+        {
+            '$sort': {
+                "averageScore": -1
+            }
+        }
+    ])
